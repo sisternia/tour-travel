a7994e64774077dc9d0ba8f4a8d714e2569ea128
sisternia <vu784512000@gmail.com>
2025-09-06 08:26:45 +0700

    Update Database (2)

--- tour-travel.sql
+++ tour-travel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th9 01, 2025 lúc 11:39 AM
+-- Thời gian đã tạo: Th9 06, 2025 lúc 03:25 AM
 -- Phiên bản máy phục vụ: 10.4.28-MariaDB
 -- Phiên bản PHP: 8.2.4
 
@@ -36,6 +36,39 @@ CREATE TABLE `users` (
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Đang đổ dữ liệu cho bảng `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_name`, `email`, `password`, `created_at`, `updated_at`) VALUES
+('f0afeb', 'vua', 'vu784512000@gmail.com', '$2b$10$GHq/DEpMAyXEC3sbka/i1eS/DcGMdFCUv54fRhEjvvO8C5TmOQDlq', '2025-09-05 16:45:07', '2025-09-05 16:45:07');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `user_infor`
+--
+
+CREATE TABLE `user_infor` (
+  `user_infor_id` varchar(10) NOT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `dob` date DEFAULT NULL,
+  `citizen_id` varchar(20) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `bio` text DEFAULT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
+  `user_id` varchar(6) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `user_infor`
+--
+
+INSERT INTO `user_infor` (`user_infor_id`, `phone`, `dob`, `citizen_id`, `address`, `bio`, `avatar`, `user_id`, `created_at`, `updated_at`) VALUES
+('eab00e57d9', NULL, NULL, NULL, NULL, NULL, NULL, 'f0afeb', '2025-09-05 16:45:07', '2025-09-05 16:45:07');
+
 -- --------------------------------------------------------
 
 --
@@ -52,6 +85,13 @@ CREATE TABLE `verify` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Đang đổ dữ liệu cho bảng `verify`
+--
+
+INSERT INTO `verify` (`verify_id`, `user_id`, `verify_code`, `verify_status`, `created_at`, `updated_at`) VALUES
+('e4cffa', 'f0afeb', '244095', 0, '2025-09-05 16:45:07', '2025-09-06 01:23:30');
+
+--
 -- Chỉ mục cho các bảng đã đổ
 --
 
@@ -63,6 +103,13 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `email` (`email`);
 
 --
+-- Chỉ mục cho bảng `user_infor`
+--
+ALTER TABLE `user_infor`
+  ADD PRIMARY KEY (`user_infor_id`),
+  ADD KEY `fk_user_infor_user` (`user_id`);
+
+--
 -- Chỉ mục cho bảng `verify`
 --
 ALTER TABLE `verify`
@@ -72,6 +119,12 @@ ALTER TABLE `verify`
 --
 -- Các ràng buộc cho các bảng đã đổ
 --
+
+--
+-- Các ràng buộc cho bảng `user_infor`
+--
+ALTER TABLE `user_infor`
+  ADD CONSTRAINT `fk_user_infor_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Các ràng buộc cho bảng `verify`
