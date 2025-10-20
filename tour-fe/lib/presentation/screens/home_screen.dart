c86b423be61d76507c0538fd42ab1a2d1cc91a67
sisternia <vu784512000@gmail.com>
2025-10-20 16:52:17 +0700

    Update Tour Screen (1)

--- tour-fe/lib/presentation/screens/home_screen.dart
+++ tour-fe/lib/presentation/screens/home_screen.dart
@@ -1,4 +1,4 @@
-// lib/presentation/screens/HomeScreen.dart
+// lib/presentation/screens/home_screen.dart
 import 'package:flutter/material.dart';
 import '../widgets/NavigationBar.dart';
 
@@ -8,6 +8,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return NavigationBarWidget(
+      currentIndex: 0,
       body: Scaffold(
         appBar: AppBar(title: const Text('Home')),
         body: const Center(child: Text('Đăng nhập thành công')),
--- tour-fe/lib/presentation/widgets/NavigationBar.dart
+++ tour-fe/lib/presentation/widgets/NavigationBar.dart
@@ -1,26 +1,70 @@
 // lib/presentation/widgets/NavigationBar.dart
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:ionicons/ionicons.dart';
+import '../screens/home_screen.dart';
+import '../screens/tourist_spot_screen.dart';
 
 class NavigationBarWidget extends StatefulWidget {
   final Widget body;
-  const NavigationBarWidget({super.key, required this.body});
+  final int currentIndex;
+  const NavigationBarWidget(
+      {super.key, required this.body, required this.currentIndex});
 
   @override
   State<NavigationBarWidget> createState() => _NavigationBarWidgetState();
 }
 
 class _NavigationBarWidgetState extends State<NavigationBarWidget> {
-  int _page = 0;
+  late int _page;
   final GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
 
   final List<Icon> _items = const [
-    Icon(Icons.home, size: 30),
-    Icon(Icons.calendar_today, size: 30),
-    Icon(Icons.message, size: 30),
-    Icon(Icons.notifications, size: 30),
-    Icon(Icons.person, size: 30),
+    Icon(Ionicons.home_outline, size: 30),
+    Icon(Ionicons.umbrella_outline, size: 30),
+    Icon(Ionicons.calendar_outline, size: 30),
+    Icon(Ionicons.chatbubble_outline, size: 30),
+    Icon(Ionicons.person_outline, size: 30),
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    _page = widget.currentIndex;
+  }
+
+  void _navigateToPage(int index) {
+    if (index == _page) return;
+
+    Widget target;
+    switch (index) {
+      case 0:
+        target = const HomeScreen();
+        break;
+      case 1:
+        target = const TouristSpotScreen();
+        break;
+      default:
+        setState(() {
+          _page = index;
+        });
+        return;
+    }
+
+    setState(() {
+      _page = index;
+    });
+    Future.delayed(const Duration(milliseconds: 600), () {
+      Navigator.pushReplacement(
+        context,
+        PageRouteBuilder(
+          pageBuilder: (_, __, ___) => target,
+          transitionDuration: Duration.zero,
+          reverseTransitionDuration: Duration.zero,
+        ),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,11 +76,7 @@ class _NavigationBarWidgetState extends State<NavigationBarWidget> {
         backgroundColor: Colors.transparent,
         color: Colors.blueAccent,
         buttonBackgroundColor: Colors.white,
-        onTap: (index) {
-          setState(() {
-            _page = index;
-          });
-        },
+        onTap: _navigateToPage,
       ),
       body: widget.body,
     );
